3d324ba1a9876efabfc489a5cdce4deabc1fa96f
Administrator <Administrator@MININT-RK0FJOP>
2018-07-26 22:02:26 +0300

    attempt 2

--- src/Task1.java
+++ src/Task1.java
@@ -5,18 +5,18 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // Объявляем Scanner
+        Scanner input = new Scanner(System.in); 
         System.out.println("Enter array length: ");
-        int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
-        int array[] = new int[size]; // Создаём массив int размером в size
+        int size = input.nextInt(); 
+        int array[] = new int[size]; 
         System.out.println("Insert array elements:");
-    /*Пройдёмся по всему массиву, заполняя его*/
+    
         for (int i = 0; i < size; i++) {
-            array[i] = input.nextInt(); // Заполняем массив элементами, введёнными с клавиатуры
+            array[i] = input.nextInt(); 
         }
         System.out.print("Inserted array elements:");
         for (int i = 0; i < size; i++) {
-            System.out.print(" " + array[i] ); // Выводим на экран, полученный массив
+            System.out.print(" " + array[i] ); 
         }
    
 
